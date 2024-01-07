6d9e543f30d23c2acd729ea38dc1e8193764a755
zhaolarry02 <66460109+zhaolarry02@users.noreply.github.com>
2024-01-06 20:08:01 -0800

    Add files via upload

---
+++ Rate-vs-SiPM_Depth.py
@@ -0,0 +1,85 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.optimize
+import os
+import re
+from Analysis import Analysis # Use functions defined in 'Analysis' python script written by Dr. Mu Wei, Dr. Mu Wei, and Larry Zhao
+
+def exp(x, m, a, b): # exponential function for curve fit
+    return (a * np.exp(np.array([-i for i in x]) * m) + b)
+
+def function(dirs):
+    depth = [60, 55, 50, 45, 40, 35, 30, 26, 25, 21, 17, 14, 12] # in inches
+    depthcm = [128.0970994, 114.9495949, 102.4368448, 89.3539062, 76.68776733, 64.50960174, 51.8504376, 42.32291091, 39.6549546, 27.42838632, 17.6438245, 9.824104101, 4.674364388] # in centimeters
+    channelID = [1, 2, 4, 5]
+    wavelength = [124, 125, 126, 127, 128, 129, 130, 131, 132, 142, 152, 173, 174, 175, 176, 177, 178, 179, 180] # in nanometers
+    wv = 128
+    ana = Analysis(1280)
+    
+    mainlist = []
+    for dir in dirs:
+        for filename in os.listdir(dir):        
+            t = re.split("_", filename) # Split file title
+            time = (int(t[3][0:2]))*3600 + int(t[3][3:5])*60 + int(t[3][6:8]) # Time in seconds since midnight
+            df = ana.import_file(dir+filename, [])
+            trigrate = ana.trig_rate(df, channelID) # Calculate Trigger Rate (Hz) using 'Analysis' script trigger rate function
+            mainlist.append([time, t[0][:2], t[1][:3], trigrate[0], trigrate[1], trigrate[2], trigrate[3]]) # sublist contains time (s), depth (in), wavelength (nm), trigger rates (Hz) for Channel 1, 2, 4, 5
+        
+    closed = [mainlist[i] for i in range(len(mainlist)) if mainlist[i][2] == 'clo'] # Shutter closed data (background signals only)
+    bkgdlist = []
+
+    for d in depth:
+        bkgd1 = [] # Ch 1
+        bkgd2 = [] # Ch 2
+        bkgd4 = [] # Ch 4
+        bkgd5 = [] # Ch 5
+        for i in range(len(closed)):
+            if float(closed[i][1]) == d:
+                bkgd1.append([closed[i][3]])
+                bkgd2.append([closed[i][4]])
+                bkgd4.append([closed[i][5]])
+                bkgd5.append([closed[i][6]])
+        bkgdlist.append([np.mean(bkgd1), np.mean(bkgd2), np.mean(bkgd4), np.mean(bkgd5)])
+
+    opened = [mainlist[i] for i in range(len(mainlist)) if mainlist[i][2] == str(wv)]
+
+    sipmz = [25.75968709, 46.25968709, 65.25968709, 143.5596871] # SiPM Depth
+    ratios = []
+    colors = ['r', 'b', 'g', 'c', 'm', 'y', 'k', 'w', 'tab:purple', 'tab:brown', 'tab:pink', 'tab:gray', 'tab:olive']
+    color = ['c', 'r', 'b', 'g']
+
+    for d in range(len(depth)):
+        signal1 = [] # Ch 1
+        signal2 = [] # Ch 2
+        signal4 = [] # Ch 3
+        signal5 = [] # Ch 4
+        wvlsignal = []
+        
+        for i in opened:
+            if float(i[1]) == depth[d]:
+                signal1.append([i[3] - bkgdlist[d][0]])
+                signal2.append([i[4] - bkgdlist[d][1]])
+                signal4.append([i[5] - bkgdlist[d][2]])
+                signal5.append([i[6] - bkgdlist[d][3]])
+        
+        sipmz2 = [depthcm[d], depthcm[d]-46.25968709+25.75968709, depthcm[d]-65.25968709+25.75968709, depthcm[d]-143.5596871+25.75968709]
+        wvlsignal.append([np.mean(signal1), np.mean(signal2), np.mean(signal4), np.mean(signal5)])
+        plt.scatter(sipmz2, wvlsignal, c=color) # , label=str(depth[d])
+        # plt.title(str(depthcm[d])+" LAr Depth (cm)")
+        # plt.ylim(ymin=0)
+    plt.title(Signal Rate vs SiPM Depth)
+    plt.xlabel('SiPM Height (cm)')
+    plt.ylabel('128 nm Trigger Rate')
+    plt.ylim(0, 25)
+        # plt.savefig("C:\\Users\\lzvio\\RatevsChannelperDepth\\"+str(depth[d])+"in. Signal vs SiPM Depth.png")
+    # plt.legend()
+    plt.show()
+        
+    # # Plot Ch 2 / Ch 1 rate
+    # ratio = np.mean(signal2) / np.mean(signal1)
+    # ratios.append(ratio)
+    # plt.scatter(depthcm, ratios)
+    # plt.title('128 nm, Ch 2/Ch 1 Rate vs Depth')
+    # plt.show()
+
+function(["C:\\Users\\lzvio\\20231110\\", "C:\\Users\\lzvio\\20231107\\", "C:\\Users\\lzvio\\20231108\\", "C:\\Users\\lzvio\\20231109\\", "C:\\Users\\lzvio\\20231111\\"])
---
+++ Waveform-vs-SSP_Output.py
@@ -0,0 +1,77 @@
+# Compares the pedestal subtracted integral calculated from waveform values to the SiPM Signal Processor (SSP)'s processed 'IntegratedSum' (Integral bound begins M2 values before trigger, length of I1)
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("C:\\...\\60tin_1790A-2_2023-07-14_14-10-02.csv") # SSP output file
+df.sort_values(by=['ChannelID'])
+
+data_files =  [ "C:\\...\\60tin_1790A_Ch1-2_2023-07-14_14-10-02.dat",
+                "C:\\...\\60tin_1790A_Ch2-2_2023-07-14_14-10-02.dat", 
+                "C:\\...\\60tin_1790A_Ch4-2_2023-07-14_14-10-02.dat", 
+                "C:\\...\\60tin_1790A_Ch5-2_2023-07-14_14-10-02.dat", 
+                "C:\\...\\60tin_1790A_Ch6-2_2023-07-14_14-10-02.dat", 
+                "C:\\...\\60tin_1790A_Ch8-2_2023-07-14_14-10-02.dat"] # Waveforms files (list of values constituting waveforms)
+dfw = pd.concat((pd.read_csv(filename, header=None) for filename in data_files))
+waveform = [dfw[0].iloc[i+24:i+2024] for i in range(0,len(dfw),2024)] # Waveforms files as list containing sublists. Each sublist contains values for one waveform.
+
+num = df[df['ChannelID']==1]['ChannelID'].count()
+
+# Initial SSP Input Settings
+I1 = 1000 # Length of 'IntegratedSum' bounds
+I2 = 200
+M1 = 40
+M2 = 20 # Length of position of 'IntegratedSum' initial bound preceding the trigger
+
+pretrig = 400 # Number of saved values preceding the trigger
+length = 2000 # Number of waveform values
+
+mean_diff = [] # Info.mean versus waveform mean
+for i in range(num):
+    mean = np.mean(waveform[i])
+    meandiff = mean / df['Info.mean'][i]
+    mean_diff.append(meandiff)
+
+int_diff = [] # IntegratedSum versus waveform integral calculation
+for i in range(num):
+    int_sum = np.sum(waveform[i][pretrig-M2:pretrig-M2+I1])
+    intdiff = int_sum / df['IntegratedSum'][i]
+    int_diff.append(intdiff)
+
+peak_diff = [] # PeakSum versus waveform peaksum calculation
+for i in range(num):
+    peak_sum = np.sum(waveform[i][pretrig:pretrig+M1])
+    peakdiff = peak_sum / df['PeakSum'][i]
+    peak_diff.append(peakdiff)
+
+pedestal_diff = []
+for i in range(num):
+    pedestal_diff.append((np.mean(waveform[i][:pretrig-M2])) / (df['Prerise'][i]/I2))
+        # pedestal_diff.append((df['Baseline'][i]/4) / (df['Prerise'][i]/I2))
+
+pedestal_sub_diff = []
+int_sub_diff = []
+for i in range(num):
+    header1 = df['IntegratedSum'][i] - (df['Prerise'][i]/I2*I1)
+    header2 = df['IntegratedSum'][i] - (np.mean(waveform[i][:pretrig-M2])*I1)
+    pedestal_sub_diff.append((header2) / (header1))
+
+    sspintprerise = (df['IntegratedSum'][i] - (df['Prerise'][i]/I2*I1))
+    sspintbaseline = (df['IntegratedSum'][i] - df['Baseline'][i]/4*I1)
+    intsubdiff = sspintprerise / header2
+    int_sub_diff.append(intsubdiff)
+
+plt.figure()
+plt.title('I1=1000, I2=200, M1=40, M2=20')
+plt.hist(pedestal_sub_diff, bins=np.linspace(0, 10, 50), alpha=0.5)
+plt.show()
+
+# Plot SiPM Trigger Waveform Example
+x = range(2000)
+y = waveform[600]
+plt.plot(x, y)
+plt.title('SiPM Trigger Waveform')
+plt.xlabel('Time Ticks (6.667 ns)')
+plt.ylabel('ADC Counts')
+plt.show()
