8ec7af4185f22c26d2c449002b5dce85c55eee96
zhaolarry02 <66460109+zhaolarry02@users.noreply.github.com>
2024-06-17 00:41:30 -0700

    Update Intsum-Peaksum-Hist.py

--- Intsum-Peaksum-Hist.py
+++ Intsum-Peaksum-Hist.py
@@ -20,6 +20,7 @@ def intsumhist(path, wvl, ch):  # path to 1107-1111Data, wvl is string e.g. 'clo
 		df = pd.concat((pd.read_csv(path+"\\"+filename, skiprows=[1]) for filename in data_files));
 		intsum  = df.loc[df['ChannelID'] == ch]['IntegratedSum'].values
 		prerise = df.loc[df['ChannelID'] == ch]['Prerise'].values
+		# intsum_minus_pedestal = df['IntegratedSum'][i] - (df['Prerise'][i]*(I1/I2))
 
 		plt.figure()
 		plt.title(str(level)+'in LAr Ch '+str(ch)+' Shutter Closed IntSum Histogram')
@@ -43,6 +44,7 @@ def peaksumhist(path, wvl, ch):  # path to 1107-1111Data, wvl is string e.g. 'cl
 		df = pd.concat((pd.read_csv(path+"\\"+filename, skiprows=[1]) for filename in data_files));
 		peaksum = df.loc[df['ChannelID'] == ch]['PeakSum'].values
 		prerise = df.loc[df['ChannelID'] == ch]['Prerise'].values
+		# peaksum_minus_pedestal = df['PeakSum'][i] - (df['Prerise'][i]*(M1/I2))
 
 		plt.figure()
 		plt.title(str(level)+'in LAr Ch '+str(ch)+' Shutter Closed PeakSum Histogram')
