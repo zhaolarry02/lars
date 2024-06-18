385088eca6b182581f997dd51d4eaffaafc6cef8
zhaolarry02 <66460109+zhaolarry02@users.noreply.github.com>
2024-06-18 00:30:03 -0700

    Update Trigrate-vs-Channel.py

--- Other/Trigrate-vs-Channel.py
+++ Other/Trigrate-vs-Channel.py
@@ -3,14 +3,13 @@ from collections import Counter
 import matplotlib.pyplot as plt
 from Analysis import Analysis
 
-# Code Plots Trigger Number or Rate per Channel For a Single Wavelength
+# Code Plots Number of Triggers per Channel For a Single Wavelength
 
 def triggerperchannel(file):
 	ana = Analysis(1280)
-	df = pd.read_csv(file)
-	df = ana.import_file(dir+filename, [])
+	df = ana.import_file(file, [])
 	counter = Counter(df['ChannelID'])
-  	trigrate = ana.trig_rate(df, channelID)
+	trigrate = ana.trig_rate(df, channelID)
 	plt.figure()
 	# plt.scatter(counter.keys(), counter.values())
 	plt.scatter(counter.keys(), trigrate)
